1975715561aca78e5ec54535f8db1399bab21169
AustinPaley <austinpaley32@gmail.com>
2018-04-27 18:19:31 -0400

    Work in progress 'active' is broken apparently.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
+require 'byebug'
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
   end
 
   private
