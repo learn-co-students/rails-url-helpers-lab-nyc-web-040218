431945763ca139399eeb8c6f46047b8cd224a23b
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-28 19:06:54 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  # def initialize(hash)
+  # super
+  # self.active = false
+  # end
+
 end
+
