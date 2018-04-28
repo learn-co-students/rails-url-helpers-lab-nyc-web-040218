71e00eef8d5e4387d7a2944c4ec15055c6873fcf
briecodes <hello@brie.work>
2018-04-27 20:20:44 -0400

    further edits.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,19 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: :show, :activate
   
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
+    # @student = Student.find(params[:id])
     if @student.active == nil
       @student.update(active: false)
     end
   end
 
   def activate
-    @student = Student.find(params[:id])
+    # @student = Student.find(params[:id])
     if @student.active == nil
     elsif @student.active == true
       @student.update(active: false)
